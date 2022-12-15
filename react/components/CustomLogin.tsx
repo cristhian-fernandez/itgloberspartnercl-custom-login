af5854dc5d9a904a45268936653f948995115aec
Cristhian Fernandez <cjfernandez29@gmail.com>
2022-12-15 18:26:22 -0500

    feat: add changed final for component custom login

--- react/components/CustomLogin.tsx
+++ react/components/CustomLogin.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useRenderSession } from 'vtex.session-client';
+import { useCssHandles } from 'vtex.css-handles';
 
-function CustomLogin() {
-    return (
-        <div>
-            Login
-        </div>
-    );
+type Props = {
+    children: any
+}
+
+function CustomLogin({ children }: Props) {
+    const { loading, session, error } = useRenderSession();
+
+    const CSS_HANDLES = [
+        "login",
+        "login__item--link"
+    ];
+    const handles = useCssHandles(CSS_HANDLES);
+
+    if (loading) return <>Session is loading</>
+    if (error) return <>Session has errors</>
+    if (session.namespaces.authentication.storeUserEmail) {
+        return (
+            <div className={`${handles["login"]}`}>
+                <div><a href="/account" className={`${handles["login__item--link"]}`}>Mi cuenta</a></div>
+                <div><a href="/account#/orders" className={`${handles["login__item--link"]}`}>Mis pedidos</a></div>
+                <div><a href="/no-cache/user/logout" className={`${handles["login__item--link"]}`}>Cerrar Sesión</a></div>
+            </div>
+        );
+    } else {
+        return (
+            <>
+                {children}
+            </>
+        );
+    }
+
 }
 
 export default CustomLogin;
